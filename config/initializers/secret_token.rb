59b69c64274fe9c038513c144f381dda9766cab7
Arnelle Balane <arnellebalane@gmail.com>
2013-08-30 14:58:12 +0800

    panimalay - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panimalay::Application.config.secret_token = '45f97f4dc849724b4e3e07ad8f21bfc7c228e7fac79bfdca632a539b4e104d298fa240a81e3dbbb8fae978125a8177a1f1b814a0e8e99c2ccfaacd4b036b8592'
