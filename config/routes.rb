cd9c473bf007dc9493046e391e350eed2d097c7f
Arnelle Balane <arnellebalane@gmail.com>
2013-09-01 20:18:19 +0800

    sessions controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Panimalay::Application.routes.draw do
+
+  resources :sessions, :only => [:new, :create, :destroy]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,7 @@ Panimalay::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
