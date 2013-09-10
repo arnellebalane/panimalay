4b455958d9a63845df00305548e129f1fd3fd738
Arnelle Balane <arnellebalane@gmail.com>
2013-09-10 18:10:45 +0800

    helper to get path to photos in public directory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :path_to_photo
+
+  def path_to_photo(filename)
+    RAILS_ROOT + "/public/" + filename
+  end
 end
