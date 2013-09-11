96d8e13f059d23c7fb05ab2199599b28990b514c
Paulinko Kiel Labian <paw2geek23@gmail.com>
2013-09-11 16:54:44 +0800

    Edited recent activities

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,12 +2,14 @@ class AccountController < ApplicationController
 
 	def index
 		@user = User.find(session[:user_id]);
-		@user_info = UserInfo.find(@user.user_info)
+		@user_info = @user.user_info
+		@activities = Post.where(user_id: @user.id).order("created_at DESC")
 	end
 
 	def show
 		@user = User.find(params[:id]);
-		@user_info = UserInfo.find(@user.user_info)
+		@user_info = @user.user_info
+		@activities = Post.where(user_id: @user.id).order("created_at DESC")
 		render :template => "account/index"
 	end
 
