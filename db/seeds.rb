221d771fadc0b35cb7d552c54edf49b18de214f3
Paulinko Kiel Labian <paw2geek23@gmail.com>
2013-10-23 22:47:31 +0800

    Added seeds for EventCategory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@ UserInfo.create(student_number: "2011-37575", firstname: "Arnelle", lastname: "B
 
 u = User.create(email: "name3anad@gmail.com", password: "pass")
 UserInfo.create(student_number: "2011-37567", firstname: "Emmanuel", lastname: "Lodovice", nickname: "Eman", course: "BS Computer Science", contact_number: "09229365294", user_id: u.id)
+
+EventCategory.create(name: "Event");
+EventCategory.create(name: "Holiday");
+EventCategory.create(name: "Birthday");
