318801495da8b74ef63d6a38046c4ebb5afea16d
Arnelle Balane <arnellebalane@gmail.com>
2013-10-25 01:53:42 +0800

    gallery page

--- app/assets/javascripts/photos.js.coffee
+++ app/assets/javascripts/photos.js.coffee
@@ -1,3 +1,16 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(document).ready ->
+  photos.initialize()
+
+photos =
+  initialize: ->
+    $("#photo-preview").click (e) ->
+      $("#photo-container input[type='file']").click()
+
+    $("#photo-container input[type='file']").change ->
+      photos.previewPhoto(this.files[0])
+
+  previewPhoto: (file) ->
+    reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = (data) ->
+      $("#photo-preview").css({"background-image": "url('#{data.target.result}')"})
--- db/schema.rb
+++ db/schema.rb
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(:version => 20130911144530) do
     t.string   "title",                                      :null => false
     t.text     "description"
     t.string   "venue",             :default => "Dormitory"
-    t.datetime "event_date"
+    t.datetime "event_date",                                 :null => false
     t.integer  "event_category_id",                          :null => false
     t.integer  "user_id",                                    :null => false
     t.datetime "created_at",                                 :null => false
@@ -247,4 +247,3 @@ ActiveRecord::Schema.define(:version => 20130911144530) do
   end
 
 end
->>>>>>> 7c54dafc39b7c8c595549ca0162fdf5459d00605
